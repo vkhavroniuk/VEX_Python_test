2d35fa7093a172430815b1a936527a7975463ccb
Volodymyr Khavroniuk <vkhavroniuk@gmail.com>
2024-12-20 09:13:58 -0800

    added PID conditions, refactored, commented

--- src/main.py
+++ src/main.py
@@ -1,41 +1,8 @@
 # Library imports
 from vex import *
-# import robot_tools
-
-# global instances
-brain = Brain()
-controller = Controller()
-brain_inertial = Inertial(Ports.PORT6)
 
 
-# MotorGroup "left_motors".
-left_motor_f = Motor(Ports.PORT10, GearSetting.RATIO_6_1, True)
-left_motor_b = Motor(Ports.PORT8, GearSetting.RATIO_6_1, True)
-left_motor_t = Motor(Ports.PORT9, GearSetting.RATIO_6_1, False)
-left_motors = MotorGroup(left_motor_f, left_motor_b, left_motor_t)
-
-# MotorGroup "right_motors".
-right_motor_f = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)
-right_motor_b = Motor(Ports.PORT12, GearSetting.RATIO_6_1, False)
-right_motor_t = Motor(Ports.PORT11, GearSetting.RATIO_6_1, True)
-
-right_motors = MotorGroup(right_motor_b, right_motor_b,right_motor_t)
-
-# smartdrive
-# smartdrive = SmartDrive(left_motors, right_motors, brain_inertial, wheelTravel, trackWidth, wheelBase, INCHES, externalGearRatio)
-# smartdrive = SmartDrive(left_motors, right_motors, brain_inertial, 156, 340, 230, MM, 0.6)
-
-
-# Clamp and Scoring mech
-clamp_pneumatic = DigitalOut(brain.three_wire_port.h)
-intake_motor = Motor(Ports.PORT7, GearSetting.RATIO_6_1, True)
-belt_motor = Motor(Ports.PORT14, GearSetting.RATIO_6_1, True)
-
-#global vars
-is_intake_spinning = False
-is_belt_spinning = False
-
-
+# PID Controller class
 class PID:
     def __init__(self, initial_eror, kp, ki, kd, start_integrate , exit_error, max_speed):
         self.kp = kp
@@ -49,24 +16,42 @@ class PID:
         self.integral = 0
     
     def calculate(self, error):
+        """
+            Calculate PID controller output
+        """
         self.error = error
+        # Proportional component
         P = self.error * self.kp
+
+        # Prevent integral windup. If error is less than start_integrate, start accumalating integral
         if abs(self.error) < self.start_integrate:
             self.integral = self.integral + self.error
         else: 
             self.integral = 0
+
+        # if error crosses 0 and changes sign, reset accumulated integral
+        if (self.prev_error < 0 and self.error > 0) or (self.prev_error > 0 and self.error < 0):
+            self.integral = 0
+
+        # calculate integral component
         I = + self.integral * self.ki
+        # calculate derivative
         D = (self.error - self.prev_error) * self.kd
+        # total gain
         total = P + I + D
         self.prev_error = self.error
+        # clamp and return
         return self.max * ( abs(P) / P ) if abs(total) > self.max else total
     
     def is_ready(self):
+        """
+            returns true if PID is settled
+        """
         if abs(self.error) < self.exit_error:
             return True
         return False
 
-# class forDrive & Turn
+# Robotclass forDrive & Turn control
 class Robot:
     def __init__(self,l_mg: MotorGroup, r_mg: MotorGroup, imu: Inertial, wheel_diameter: float, gear_ratio: float):
         self.inertial = imu
@@ -74,9 +59,37 @@ class Robot:
         self.right_motors = r_mg
         self.wheel_diameter = wheel_diameter
         self.gear_ratio = gear_ratio
+        self.drive_def_kp = 0
+        self.drive_def_ki = 0
+        self.drive_def_kd = 0
+        self.drive_def_si = 0
+        self.drive_def_exit = 0
+
+    def set_drive_defaults(self, kp, ki, kd, start_integrate, exit_error):
+        """
+            set default PID parameters for drive
+        """
+        self.drive_def_kp = kp
+        self.drive_def_ki = ki
+        self.drive_def_kd = kd
+        self.drive_def_si = start_integrate
+        self.drive_def_exit = exit_error
+
+    def set_turn_defaults(self, kp, ki, kd, start_integrate, exit_error, max_speed):
+        """
+            set default PID parameters for turn
+        """        
+        self.turn_def_kp = kp
+        self.turn_def_ki = ki
+        self.turn_def_kd = kd
+        self.turn_def_si = start_integrate
+        self.turn_def_exit = exit_error
 
     @staticmethod
     def angle_optimizer(angle: float):
+        """
+            if robot requested to turn more than 180 degress, then it turns to opposite side to save time
+        """
         angle = angle % 360
         if angle < -180:
             angle = angle + 360
@@ -85,6 +98,9 @@ class Robot:
         return angle
 
     def reset_encoders(self):
+        """
+            Resets motor encoders
+        """
         self.left_motors.reset_position()
         self.right_motors.reset_position()
 
@@ -94,14 +110,16 @@ class Robot:
         """
         return ((self.left_motors.position(TURNS) + self.right_motors.position(TURNS)) / 2) * (self.wheel_diameter * 3.14) * self.gear_ratio
 
-    def drive_for(self, distance, max_power = 8, heading = -1):
+    def drive_for(self, distance, max_speed = 8, heading = -1):
+        """
+            Robot Drives to specified distance with specified max_speed. If heading is not specified it drives staight
+        """       
         if heading == -1:
             heading = self.inertial.heading()
-        # self.left_motors.reset_position()
-        # self.left_motors.reset_position()
+
         self.reset_encoders()
 
-        drive_pid = PID(distance, 1.5, 0, max_power ,1 , 1, 6)
+        drive_pid = PID(distance, self.drive_def_kp, self.drive_def_ki, self.drive_def_kd , self.drive_def_si , self.drive_def_exit, max_speed)
         steer_pid = PID(0, 0.4, 0, 1, 0 , 1, 6)
 
         while not drive_pid.is_ready():
@@ -124,20 +142,53 @@ class Robot:
             # end of temporary debug output
 
             # set motor speed
-            self.left_motors.spin(FORWARD, drive_speed * 8.3  , VelocityUnits.PERCENT)
-            self.right_motors.spin(FORWARD, drive_speed * 8.3 , VelocityUnits.PERCENT)
+            self.left_motors.spin(FORWARD, drive_speed * 8.3 - heading_correction_speed  , VelocityUnits.PERCENT)
+            self.right_motors.spin(FORWARD, drive_speed * 8.3 + heading_correction_speed , VelocityUnits.PERCENT)
             wait(20, MSEC)
 
         self.left_motors.spin(FORWARD, 0)
         self.right_motors.spin(FORWARD, 0)        
 
+    def turn_to(self, heading):
+        pass
+
+# Define all required devices
+brain = Brain()
+controller = Controller()
+brain_inertial = Inertial(Ports.PORT6)
+
+# MotorGroup "left_motors".
+left_motor_f = Motor(Ports.PORT10, GearSetting.RATIO_6_1, True)
+left_motor_b = Motor(Ports.PORT8, GearSetting.RATIO_6_1, True)
+left_motor_t = Motor(Ports.PORT9, GearSetting.RATIO_6_1, False)
+left_motors = MotorGroup(left_motor_f, left_motor_b, left_motor_t)
+
+# MotorGroup "right_motors".
+right_motor_f = Motor(Ports.PORT13, GearSetting.RATIO_6_1, True)
+right_motor_b = Motor(Ports.PORT12, GearSetting.RATIO_6_1, False)
+right_motor_t = Motor(Ports.PORT11, GearSetting.RATIO_6_1, True)
+
+right_motors = MotorGroup(right_motor_b, right_motor_b,right_motor_t)
+
+# Clamp and Scoring mech
+clamp_pneumatic = DigitalOut(brain.three_wire_port.h)
+intake_motor = Motor(Ports.PORT7, GearSetting.RATIO_6_1, True)
+belt_motor = Motor(Ports.PORT14, GearSetting.RATIO_6_1, True)
+
+#global variables
+is_intake_spinning = False
+is_belt_spinning = False
+
 # create my_robot object for autonomous
 my_robot = Robot(left_motors, right_motors, brain_inertial, 3.25, 0.6)
+
 
 def set_initial_params():
     """ Set initial params
     """
     brain_inertial.set_heading(90)
+    # set Kp, Ki, Kd, start integrate, exit error, max_speed
+    my_robot.set_drive_defaults(1.5, 0, 8, 1, 1)
 
 
 def calibrate_inertial():
@@ -180,7 +231,8 @@ def clamp():
         clamp_pneumatic.set(False) 
 
 def intake(dir = FORWARD, spd = 90):
-    """ Start and Stop Intake
+    """ 
+        Starts and Stops Intake
     """
     global is_intake_spinning
     if not is_intake_spinning:
@@ -192,7 +244,8 @@ def intake(dir = FORWARD, spd = 90):
     
 
 def belt(dir = FORWARD, spd = 90):
-    """ Start Belt
+    """ 
+        Starts and Stops Belt
     """
     global is_belt_spinning
     if not is_belt_spinning:
@@ -204,44 +257,46 @@ def belt(dir = FORWARD, spd = 90):
 
 
 def score():
-    """ Start Intake and Belt
+    """ 
+        Button callback. Starts Intake and Belt
     """
     intake()
     belt()
 
 def autonomous():
-
+    """
+        Autonomous Code
+    """
     my_robot.drive_for(25)
     wait(1, SECONDS)
     my_robot.drive_for(-25)
 
 def user_control():
-    brain.screen.clear_screen()
-    brain.screen.print("driver control")
-    # place driver control in this while loop
+    """
+        Driver Control Code
+    """    
     while True:
-
         throttle = controller.axis3.position()
         turn = controller.axis1.position()
         left_motors.spin(FORWARD, throttle + turn,VelocityUnits.PERCENT)
         right_motors.spin(FORWARD, throttle - turn ,VelocityUnits.PERCENT)
-
         wait(20, MSEC)
 
 # create competition instance
 comp = Competition(user_control, autonomous)
 
-# actions to do when the program starts
+# Clear Screen
 brain.screen.clear_screen()
 
-# bind buttons
+# bind button callback functions
 controller.buttonL1.pressed(clamp)
 controller.buttonR1.pressed(score)
 
-# set params, calibrate inertial
+# set PID params, calibrate inertial
 calibrate_inertial()
 set_initial_params()
 
+# start parallel thread for info output
 thread = Thread(show_me_info)
 
 
